49d0b35e160faad94a1a6945e161d3874309b5ea
Carlo Pinciroli <carlo@pinciroli.net>
2021-08-25 21:12:56 -0400

    More work on the model

--- SoftKilobot.jl
+++ SoftKilobot.jl
@@ -1,15 +1,17 @@
 module SoftKilobot
 
-using DifferentialEquations
-using Distributions
+########################################
+
 using LinearAlgebra
-using Random
+using Maybe
 using Plots
-gr();
+
+########################################
 
 export Alg_Idx
 export Alg_P
-export Alg_Nbrs
+export Alg_NbrsDs
+export Alg_NbrsAs
 export Init
 export Alg_Dist
 export Alg_Robot
@@ -20,6 +22,7 @@ export UpdateState
 export Step!
 export CoM
 export Plot
+export PlotTrajectories
 export ENV_SIDE
 export ENV_START
 export ENV_END
@@ -38,13 +41,29 @@ export KILOBOT_RADIUS
 export KILOBOT_AXLE
 export KILOBOT_VMAX
 
+########################################
+
 # Environment params
 ENV_SIDE  = 2.0;
 ENV_START = 0.4;
 ENV_END   = 1.6;
 
+# Algorithm parameters
+ALG_DIST_159_1      = 0.01; # 1st deformation test
+ALG_DIST_159_2      = 0.04; #0.07; # 2nd deformation test
+ALG_ANGL_1          = 85.0 * pi / 180.0
+ALG_ANGL_9          = 95.0 * pi / 180.0
+ALG_ANGL_5_1        = 95.0 * pi / 180.0
+ALG_ANGL_5_2        = 85.0 * pi / 180.0
+ALG_DIST_234678_1_1 = 0.11; # 1st deformation test
+ALG_DIST_234678_1_2 = 0.10; # 1st deformation test
+ALG_DIST_234678_2   = 0.07; # 2nd deformation test
+ALG_ANGL_234678_1   = 85.0 * pi / 180.0
+ALG_ANGL_234678_2   = 95.0 * pi / 180.0
+
 # Control frequency
-DELTA_T = 1.0 / 31.0;
+STEPS_PER_SEC = 31
+DELTA_T = 1.0 / STEPS_PER_SEC;
 
 # Motion state field
 VEL_X   = 0;
@@ -63,7 +82,11 @@ RWHEEL = 2;
 KILOBOT_MASS   = 0.024;
 KILOBOT_RADIUS = 0.02;
 KILOBOT_AXLE   = 0.025;
-KILOBOT_VMAX   = 0.015;
+KILOBOT_WMAX   = 0.015; # Max wheel speed
+KILOBOT_FMAX   = 0.5;   # Max linear force
+KILOBOT_TMAX   = 0.5;   # Max torque
+
+########################################
 
 """
 The SoftKilobot state information.
@@ -74,7 +97,9 @@ mutable struct State
   MASS::Float64                  # Robot mass [kg]
   RADIUS::Float64                # Robot radius [m]
   AXLE::Float64                  # Robot wheel axle [m]
-  V_MAX::Float64                 # Max speed
+  W_MAX::Float64                 # Max wheel speed
+  F_MAX::Float64                 # Max linear force
+  T_MAX::Float64                 # Max torque
   V_DAMP::Float64                # Speed damping
   S_REST::Float64                # Spring rest length [m]
   S_STIFF::Float64               # Spring stiffness
@@ -83,24 +108,30 @@ mutable struct State
   n::Int                         # Number of robots
   I::Float64                     # Moment of inertia
   nbr_springs::Array{Array{Int}} # Neighbors due to spring linkage
-  nbr_alg::Array{Array{Int}}     # Neighbors according to algorithm, see Fig. 8
+  p_alg::Array{Int}              # P index according to algorithm
+  nbr_alg_ds::Array{Array{Int}}  # Neighbors to calculate the d's, see Fig. 8
+  nbr_alg_as::Array{Array{Int}}  # Neighbors to calculate the a's, see Fig. 8
 
   vs::Matrix{Float64}            # Linear and rotational velocity in the world frame
   w_bias::Matrix{Float64}        # Bias on wheel speed
   w_speeds::Matrix{Float64}      # Wheel speeds
-  stept::Array{Float64}          # Time for the next control step
-  State(n_side,mass,radius,axle,v_max,v_damp,s_rest,s_stiff,s_damp) = (
-    y = new(n_side,mass,radius,axle,v_max,v_damp,s_rest,s_stiff,s_damp);
+  step_t::Array{Float64}         # Time for the next control step
+  State(n_side,mass,radius,axle,w_max,f_max,t_max,v_damp,s_rest,s_stiff,s_damp) = (
+    y = new(n_side,mass,radius,axle,w_max,f_max,t_max,v_damp,s_rest,s_stiff,s_damp);
     y.n = n_side^2;
     y.I = 0.5 * y.MASS * y.RADIUS^2;
     y.nbr_springs = [ Int[] for k in 1:y.n ];
-    y.nbr_alg = [ Int[] for k in 1:y.n ];
+    y.p_alg = zeros(Int64, y.n);
+    y.nbr_alg_ds = [ Int[] for k in 1:y.n ];
+    y.nbr_alg_as = [ Int[] for k in 1:y.n ];
     y.vs = zeros(3, y.n);
     y.w_bias = zeros(2, y.n);
-    y.w_speeds = zeros(2, y.n);
-    y.stept = zeros(y.n);
+    y.w_speeds = fill(w_max, (2, y.n));
+    y.step_t = zeros(y.n);
     return y)
 end;
+
+########################################
 
 """
 Get a robot's index given its coordinates in the lattice.
@@ -133,10 +164,12 @@ function Alg_P(
   if i == n_side; return 6; end
 end;
 
+########################################
+
 """
-The indices of the neighbors, as defined in Fig. 8 of the paper.
+The indices of the neighbors to calculate the d's, as defined in Fig. 8 of the paper.
 """
-function Alg_Nbrs(
+function Alg_NbrsDs(
   i::Int, j::Int, # The coordinates of the current robot
   n_side::Int)    # The number of robots on the side
   p = Alg_P(i,j,n_side)
@@ -175,6 +208,37 @@ function Alg_Nbrs(
 end;
 
 """
+The indices of the neighbors to calculate the a's, as defined in Fig. 8 of the paper.
+"""
+function Alg_NbrsAs(
+  i::Int, j::Int, # The coordinates of the current robot
+  n_side::Int)    # The number of robots on the side
+  p = Alg_P(i,j,n_side)
+  # Front robot
+  if p == 1
+    return [Alg_Idx(i+1, j, n_side), Alg_Idx(i, j+1, n_side)]
+  end
+  # Tail robot
+  if p == 9
+    return [Alg_Idx(i-1, j, n_side), Alg_Idx(i, j-1, n_side)]
+  end
+  # Inner robots
+  if p == 5
+    return [Alg_Idx(i+1, j, n_side), Alg_Idx(i, j+1, n_side), Alg_Idx(i-1, j, n_side), Alg_Idx(i, j-1, n_side)]
+  end
+  # North east robots
+  if p == 2 || p == 3 || p == 6
+    return [Alg_Idx(i-1, j, n_side), Alg_Idx(i, j+1, n_side)]
+  end
+  # South west robots
+  if p == 4 || p == 7 || p == 8
+    return [Alg_Idx(i+1, j, n_side), Alg_Idx(i, j-1, n_side)]
+  end
+end;
+
+########################################
+
+"""
 SoftKilobot initialization function.
 1. Places the robots
 2. Creates the spring connections
@@ -189,14 +253,16 @@ function Init(
   mass::Float64,         # Robot mass [kg]
   radius::Float64,       # Robot radius [m]
   axle::Float64,         # Robot wheel axle [m]
-  v_max::Float64,        # Max speed [m/s]
+  w_max::Float64,        # Max wheel speed [m/s]
+  f_max::Float64,        # Max force [N]
+  t_max::Float64,        # Max torque [Nm]
   v_damp::Float64,       # Speed damping
   w_noise,               # Noise on wheel speed
   s_rest::Float64,       # Spring rest length
   s_stiff::Float64,      # Spring stiffness
   s_damp::Float64)       # Spring damping
   # Create empty spring system
-  ss = State(n_side,mass,radius,axle,v_max,v_damp,s_rest,s_stiff,s_damp)
+  ss = State(n_side,mass,radius,axle,w_max,f_max,t_max,v_damp,s_rest,s_stiff,s_damp)
   # Add spring links
   for k in 1:ss.n
     # Horizontal
@@ -211,7 +277,9 @@ function Init(
   # Add algorithm neighbors
   for i in 1:n_side
     for j in 1:n_side
-      ss.nbr_alg[Alg_Idx(i, j, n_side)] = Alg_Nbrs(i, j, n_side)
+      ss.p_alg[Alg_Idx(i, j, n_side)] = Alg_P(i, j, n_side);
+      ss.nbr_alg_ds[Alg_Idx(i, j, n_side)] = Alg_NbrsDs(i, j, n_side);
+      ss.nbr_alg_as[Alg_Idx(i, j, n_side)] = Alg_NbrsAs(i, j, n_side);
     end
   end
   # Calculate rotation matrix
@@ -233,56 +301,87 @@ function Init(
       x[:, k] = [0.0 0.0 0.0 px[1] px[2] 0.0]
       # Wheel noise
       if(w_noise != nothing)
-        ss.w_bias[LWHEEL, k] = rand(w_noise)
-        ss.w_bias[RWHEEL, k] = rand(w_noise)
+        @? ss.w_bias[LWHEEL, k] = rand(w_noise)
+        @? ss.w_bias[RWHEEL, k] = rand(w_noise)
       end
     end
   end
   return ss, x
 end;
 
+########################################
+
+function Alg_GetPos(
+  k::Int,             # The index of the current robot
+  u::Vector{Float64}, # The motion state of the kilobots
+  ss::State)          # The SoftKilobot
+  i = (k-1) * FIELDS + 1
+  return u[POS_X + i:POS_Y + i]
+end
+
 """
 Calculates the distances as defined in Fig. 8 of the paper.
 """
 function Alg_Dist(
-  i::Int,j::Int,      # The coordinates of the current robot
+  k::Int,             # The index of the current robot
   u::Vector{Float64}, # The motion state of the kilobots
   ss::State)          # The SoftKilobot
-  # Index of this robot
-  k = Alg_Idx(i, j, ss.N_SIDE)
   # Position vector of this robot
-  bidx = (k-1) * FIELDS + 1
-  x = u[POS_X + bidx:POS_Y + bidx]
+  x = Alg_GetPos(k, u, ss)
   # Make list of distances
   ds = []
-  for nbr in ss.nbrs_alg[k]
-    # Position vector of neighbor
-    bidx = (nbr-1) * FIELDS + 1
-    nx = u[POS_X + bidx:POS_Y + bidx]
-    # Calculate distance
-    append!(ds, norm(x - nx))
+  for nbr in ss.nbr_alg_ds[k]
+    push!(ds, norm(x - Alg_GetPos(nbr, u, ss)))
   end
   return ds
 end;
 
 """
-The SoftKilobot motion control algorithm.
-Returns the target speed of the robot.
+Calculates the angle given three points.
+The angle is referred wrt p.
 """
-function Alg_Robot(
+function Alg_CalcAngle(p, n1, n2)
+  v1 = n1 - p
+  v2 = n2 - p
+  return acos(dot(v1, v2) / (norm(v1) * norm(v2)))
+end
+
+"""
+Calculates the angles as defined in Fig. 8 of the paper.
+"""
+function Alg_Angles(
   k::Int,             # The index of the current robot
   u::Vector{Float64}, # The motion state of the kilobots
-  ss::State,          # The SoftKilobot
-  t::Float64)         # The current time
-  vs_tgt = [0.1; 0.0]
-  #    if ss.t[k] <= t
-  #    end
-  return vs_tgt
+  ss::State)          # The SoftKilobot
+  # Position vector of this robot
+  x = Alg_GetPos(k, u, ss)
+  # Make list of angles
+  as = []
+  if length(ss.nbr_alg_as[k]) == 2
+    # Position vectors of the neighbors
+    nx1 = Alg_GetPos(ss.nbr_alg_as[k][1], u, ss)
+    nx2 = Alg_GetPos(ss.nbr_alg_as[k][2], u, ss)
+    # Calculate angle
+    push!(as, Alg_CalcAngle(x, nx1, nx2))
+  else
+    # Position vectors of the neighbors
+    nx1 = Alg_GetPos(ss.nbr_alg_as[k][1], u, ss)
+    nx2 = Alg_GetPos(ss.nbr_alg_as[k][2], u, ss)
+    # Calculate angle
+    push!(as, Alg_CalcAngle(x, nx1, nx2))
+    # Position vectors of the neighbors
+    nx1 = Alg_GetPos(ss.nbr_alg_as[k][3], u, ss)
+    nx2 = Alg_GetPos(ss.nbr_alg_as[k][4], u, ss)
+    # Calculate angle
+    push!(as, Alg_CalcAngle(x, nx1, nx2))
+  end
+  return as
 end;
+
+########################################
 
 """
 The SoftKilobot motion control algorithm.
-Returns a Matrix{Float64}.
 """
 function Alg(
   u::Vector{Float64}, # The motion state of the kilobots
@@ -291,12 +390,70 @@ function Alg(
   # By default, everybody keeps their old wheel speeds
   # For each robot
   #   If it's time to update the speeds, do it
-  vs_tgt = zeros(2,ss.n)
   for k in 1:ss.n
-    vs_tgt[:,k] = Alg_Robot(k,u,ss,t)
+    if t > ss.step_t[k]
+      # Move forward by default
+      ss.w_speeds[:,k] = [KILOBOT_WMAX; KILOBOT_WMAX];
+      ss.step_t[k] = t + 0.5;
+      dsts = Alg_Dist(k, u, ss);
+      angls = Alg_Angles(k, u, ss);
+      # Position-based corrections
+      if ss.p_alg[k] in [1,5,9]
+        # Head/tail/interior robot
+        d12 = dsts[1] - dsts[2];
+        if(d12 > ALG_DIST_159_1)
+          # turn right
+          ss.w_speeds[:,k] = [KILOBOT_WMAX; -KILOBOT_WMAX];
+        elseif(d12 < -ALG_DIST_159_1)
+          # turn left
+          ss.w_speeds[:,k] = [-KILOBOT_WMAX; KILOBOT_WMAX];
+        elseif(
+          ((ss.p_alg[k] == 1) && (angls[1] < ALG_ANGL_1) && (dsts[1] > ALG_DIST_159_2) && (dsts[2] > ALG_DIST_159_2)) ||
+          ((ss.p_alg[k] == 9) && (angls[1] > ALG_ANGL_9) && (dsts[1] < ALG_DIST_159_2) && (dsts[2] < ALG_DIST_159_2)) ||
+          ((ss.p_alg[k] == 5) && ((angls[1] > ALG_ANGL_5_1) || (angls[1] < ALG_ANGL_5_2)))
+          )
+          # Stop movement
+          ss.w_speeds[:,k] = [0.0; 0.0];
+        end
+      else
+        # Edge robots
+        if(ss.p_alg[k] == 2)
+          println("  angls[1] = ", angls[1] * 180.0 / pi);
+          println("  dsts[1]  = ", dsts[1]);
+          println("  dsts[2]  = ", dsts[2]);
+        end
+        # Assume left-hand side robots by default
+        turn = [KILOBOT_WMAX; -KILOBOT_WMAX];
+        if ss.p_alg[k] in [2,3,6]
+          # Right-hand side robots
+          turn = -turn;
+        end
+        # Deformation test
+        if ((angls[1] < ALG_ANGL_234678_1) || (dsts[1] > ALG_DIST_234678_1_1))
+          # Turn towards center
+          ss.w_speeds[:,k] = turn;
+          if(ss.p_alg[k] == 2)
+            println("  TURN TOWARDS CENTER: ", turn);
+          end
+        elseif ((angls[1] > ALG_ANGL_234678_2) || (dsts[1] < ALG_DIST_234678_1_2))
+          # Turn away from center
+          ss.w_speeds[:,k] = -turn;
+          if(ss.p_alg[k] == 2)
+            println("  TURN AWAY FROM CENTER: ", -turn);
+          end
+        elseif((dsts[2] < ALG_DIST_234678_2) || (dsts[3] < ALG_DIST_234678_2))
+          # Stop movement
+          ss.w_speeds[:,k] = [0.0; 0.0];
+          if(ss.p_alg[k] == 2)
+            println("  STOP");
+          end
+        end
+      end
+    end
   end
-  return vs_tgt
 end;
+
+########################################
 
 """
 Calculates the forces between robots in the SoftKilobot.
@@ -354,7 +511,6 @@ function UpdateState(
   du::Vector{Float64},     # Difference in motion state of the kilobots
   u::Vector{Float64},      # Motion state of the kilobots
   ss::State,               # The SoftKilobot
-  ws_tgt::Matrix{Float64}, # Target wheel speeds [m/s]
   fs::Matrix{Float64})     # Forces [N]
   for k in 1:ss.n
     # Robot index in state vector to identify block
@@ -365,20 +521,18 @@ function UpdateState(
     vw_old = u[VEL_W + idx]
     # Calculate linear speeds
     vx, vy, vw = DiffSteering(
-      ws_tgt[LWHEEL,k] + ss.w_bias[1,k],
-      ws_tgt[RWHEEL,k] + ss.w_bias[2,k],
+      ss.w_speeds[LWHEEL,k] + ss.w_bias[1,k],
+      ss.w_speeds[RWHEEL,k] + ss.w_bias[2,k],
       u[ORNT + idx],
       ss.AXLE)
     # Calculate velocity update
-    ss.vs[1,k] = clamp(-ss.V_DAMP * vx_old + (vx - vx_old + fs[1,k]) / ss.MASS, -ss.V_MAX, ss.V_MAX)
-    ss.vs[2,k] = clamp(-ss.V_DAMP * vy_old + (vy - vy_old + fs[2,k]) / ss.MASS, -ss.V_MAX, ss.V_MAX)
+    ss.vs[1,k] = -ss.V_DAMP * vx_old + (vx - vx_old + fs[1,k]) / ss.MASS
+    ss.vs[2,k] = -ss.V_DAMP * vy_old + (vy - vy_old + fs[2,k]) / ss.MASS
+    ss.vs[1,k] = clamp(ss.vs[1,k], -ss.F_MAX, ss.F_MAX)
+    ss.vs[2,k] = clamp(ss.vs[2,k], -ss.F_MAX, ss.F_MAX)
     # State update
-    if ss.vs[1,k] > -ss.V_MAX && ss.vs[1,k] < ss.V_MAX
-      du[VEL_X + idx] = ss.vs[1,k]
-    end
-    if ss.vs[2,k] > -ss.V_MAX && ss.vs[2,k] < ss.V_MAX
-      du[VEL_Y + idx] = ss.vs[2,k]
-    end
+    du[VEL_X + idx] = ss.vs[1,k] - vx_old
+    du[VEL_Y + idx] = ss.vs[2,k] - vy_old
     du[VEL_W + idx] = (vw - vw_old) / ss.I
     du[POS_X + idx] = vx_old
     du[POS_Y + idx] = vy_old
@@ -386,6 +540,8 @@ function UpdateState(
   end
   return du
 end;
+
+########################################
 
 """
 Step function to calculate the new state of the SoftKilobot.
@@ -396,14 +552,16 @@ function Step!(
   ss, # The SoftKilobot
   t)  # Time [s]
   # Calculate target speeds using algorithm
-  ws_tgt = Alg(u, ss, t)
+  Alg(u, ss, t)
   # Calculate forces due to springs
-  fs = CalcForces(u, ss)
+  fs = CalcForces(u, ss);
   # Update states
-  du = UpdateState(du, u, ss, ws_tgt, fs)
+  du = UpdateState(du, u, ss, fs)
   # All done
   return du
 end;
+
+########################################
 
 """
 Calculates the center of mass of a SoftKilobot
@@ -422,6 +580,8 @@ function CoM(
   return com / ss.n
 end;
 
+########################################
+
 """
 Plots the SoftKilobot
 """
@@ -431,44 +591,67 @@ function Plot(
   t::Float64;         # The current time
   vel=false,          # Whether to plot the velocity vectors
   nbr_springs=false,  # Whether to plot the springs
-  nbr_alg=false)      # Whether to plot the algorithm-related links
-    # General settings
-    p = plot(title=string("t = ", t), show=false)
-    plot!(p, xlims=(0,2), ylims=(0,2), legend=:none)
-    # Start and end lines
-    plot!(p, [ENV_START ENV_END; ENV_START ENV_END], [0 0; ENV_SIDE ENV_SIDE])
-    # Robots as circles
-    scatter!(p, x[POS_X+1,:], x[POS_Y+1,:])
-    # Plot velocities
-    if(vel)
-        for i in 1:ss.n
-            # New velocity
-            plot!(p, [x[POS_X+1,i]; (x[POS_X+1,i]+x[VEL_X+1,i])], [x[POS_Y+1,i]; (x[POS_Y+1,i]+x[VEL_Y+1,i])], label="old")
-            # Old velocity
-            plot!(p, [x[POS_X+1,i]; (x[POS_X+1,i]+ss.vs[1,i])], [x[POS_Y+1,i]; (x[POS_Y+1,i]+ss.vs[2,i])], label="new")
+  nbr_alg_ds=false,   # Whether to plot the algorithm-related distance links
+  nbr_alg_as=false)   # Whether to plot the algorithm-related angle links
+  # General settings
+  p = plot(title=string("t = ", t), legend=:none)
+  plot!(p, xlims=(0,2), ylims=(0,2))
+  # Start and end lines
+  plot!(p, [ENV_START ENV_END; ENV_START ENV_END], [0 0; ENV_SIDE ENV_SIDE])
+  # Robots as circles
+  scatter!(p, x[POS_X+1,:], x[POS_Y+1,:])
+  # Plot velocities
+  if(vel)
+    i = 2
+    plot!(p, [x[POS_X+1,i]; (x[POS_X+1,i]+x[VEL_X+1,i]*100)], [x[POS_Y+1,i]; (x[POS_Y+1,i]+x[VEL_Y+1,i]*100)], label="new")
+    # for i in 1:ss.n
+    #   # New velocity
+    #   plot!(p, [x[POS_X+1,i]; (x[POS_X+1,i]+x[VEL_X+1,i]*100)], [x[POS_Y+1,i]; (x[POS_Y+1,i]+x[VEL_Y+1,i]*100)], label="new")
+    #   # Old velocity
+    #   plot!(p, [x[POS_X+1,i]; (x[POS_X+1,i]+ss.vs[1,i])], [x[POS_Y+1,i]; (x[POS_Y+1,i]+ss.vs[2,i])], label="old")
+    # end
+  end
+  # Plot springs as segments
+  if(nbr_springs)
+    for i in 1:ss.n
+      for j in ss.nbr_springs[i]
+        if(i < j)
+          plot!(p, [x[POS_X+1,i]; x[POS_X+1,j]], [x[POS_Y+1,i]; x[POS_Y+1,j]])
         end
+      end
     end
-    # Plot springs as segments
-    if(nbr_springs)
-        for i in 1:ss.n
-            for j in ss.nbr_springs[i]
-                if(i < j)
-                    plot!(p, [x[POS_X+1,i]; x[POS_X+1,j]], [x[POS_Y+1,i]; x[POS_Y+1,j]])
-                end
-            end
-        end
+  end
+  # Plot algorithm neighbors as segments
+  if(nbr_alg_ds)
+    for i in 1:ss.n
+      for j in ss.nbr_alg_ds[i]
+        plot!(p, [x[POS_X+1,i]; x[POS_X+1,j]], [x[POS_Y+1,i]; x[POS_Y+1,j]])
+      end
     end
-    # Plot algorithm neighbors as segments
-    if(nbr_alg)
-        for i in 1:ss.n
-            for j in ss.nbr_alg[i]
-                if(i < j)
-                    plot!(p, [x[POS_X+1,i]; x[POS_X+1,j]], [x[POS_Y+1,i]; x[POS_Y+1,j]])
-                end
-            end
-        end
+  end
+  # Plot algorithm neighbors as segments
+  if(nbr_alg_as)
+    for i in 1:ss.n
+      for j in ss.nbr_alg_as[i]
+        plot!(p, [x[POS_X+1,i]; x[POS_X+1,j]], [x[POS_Y+1,i]; x[POS_Y+1,j]])
+      end
     end
-    return p
+  end
+  return p
 end;
 
-end
+########################################
+
+function PlotTrajectories(
+  x::Matrix{Float64}, # The poses of the SoftKilobot robots
+  ss::State)          # The SoftKilobot
+    # General settings
+    p = plot(title=string("t = ", t))
+    plot!(p, xlims=(0,2), ylims=(0,2), legend=:none)
+    # Start and end lines
+    plot!(p, [ENV_START ENV_END; ENV_START ENV_END], [0 0; ENV_SIDE ENV_SIDE])  
+end;
+
+########################################
+
+end # module SoftKilobot
--- main.jl
+++ main.jl
@@ -1,16 +1,30 @@
+using DifferentialEquations
+using Distributions
+using Plots
+
+# See https://discourse.julialang.org/t/plots-gr-png-results-in-too-many-open-files/48998/11
+ENV["GKSwstype"] = "100"
+
 include("SoftKilobot.jl")
 import .SoftKilobot as SK
 
-##
 # Duration in seconds
-DURATION = 0.25;
+#DURATION = SK.DELTA_T;
+#DURATION = 800
+DURATION = 120
+#DURATION = 1
 
+# Whether the simulation is done
 function Done(
   u::Vector{Float64}, # Motion state of the kilobots
   ss::SK.State,       # The SoftKilobot
   t::Integer)         # Time step
   return ((t > DURATION/SK.DELTA_T) || (SK.CoM(u, ss)[1] > SK.ENV_END))
 end;
+
+# Noise
+#NOISE = nothing
+NOISE = Some(Normal(SK.KILOBOT_WMAX * 0.001, SK.KILOBOT_WMAX * 0.001))
 
 # Initialization
 ss, u0 = SK.Init(
@@ -20,62 +34,44 @@ ss, u0 = SK.Init(
   SK.KILOBOT_MASS,   # Robot mass
   SK.KILOBOT_RADIUS, # Robot radius [m]
   SK.KILOBOT_AXLE,   # Robot wheel axle [m]
-  SK.KILOBOT_VMAX,   # Max speed
-  0.01,              # Speed damping
-  nothing,           # Noise on wheel speed
+  SK.KILOBOT_WMAX,   # Max wheel speed
+  SK.KILOBOT_FMAX,   # Max linear force
+  SK.KILOBOT_TMAX,   # Max torque
+  0.99,              # Speed damping
+  NOISE,             # Noise on wheel speed
   0.031,             # Spring rest length
-  0.6,               # Spring stiffness
+  0.1, #0.6,               # Spring stiffness
   0.1);              # Spring damping
 u = vec(u0);
+println("Initialized");
 
 # Execution
-anim = Animation()
 i = 0
+steps = []
 while (!Done(u, ss, i))
   global i
   tspan = (i * SK.DELTA_T, (i+1) * SK.DELTA_T);
   prob = ODEProblem(SK.Step!, u, tspan, ss);
   sol = solve(prob);
-  for j in 1:length(sol.t)
-    fr = SK.Plot(
-      reshape(sol.u[j], SK.FIELDS, ss.n),
-      ss,
-      sol.t[j],
-      vel=true,
-      nbr_springs=false,
-      nbr_alg=false);
-    frame(anim, fr);
-  end
+  push!(steps, sol);
   global u = sol.u[end]
   i += 1
+  println("Step #", i);
 end
-gif(anim, "no_noise.gif", fps=31);
+println("Done with execution");
 
-# # Initialization
-# ss, u0 = SK.Init(
-#     [0.4,1],                                                 # Center of the robot distribution
-#     135.0,                                                   # Orientation of robot distribution (degrees)
-#     1,                                                       # Robots per side
-#     SK.KILOBOT_MASS,                                         # Robot mass
-#     SK.KILOBOT_RADIUS,                                       # Robot radius [m]
-#     SK.KILOBOT_AXLE,                                         # Robot wheel axle [m]
-#     SK.KILOBOT_VMAX,                                         # Max speed
-#     0.01,                                                    # Speed damping
-#     Some(Normal(SK.KILOBOT_VMAX*0.1, SK.KILOBOT_VMAX*0.01)), # Noise on wheel speed
-#     0.031,                                                   # Spring rest length
-#     0.6,                                                     # Spring stiffness
-#     0.1)                                                     # Spring damping
-# u = vec(u0)
-
-# # Execution
-# i = 0
-# @gif while (!Done(u, ss, i))
-#     tspan = (i * SK.DELTA_T, (i+1) * SK.DELTA_T);
-#     prob = ODEProblem(SK.Step!, u, tspan, ss);
-#     sol = solve(prob)
-#     for j in 1:length(sol.t)
-#         SK.Plot(reshape(sol.u[j], FIELDS, ss.n), sol.t[j], vel=true, nbr_springs=false, nbr_alg=false)
-#     end
-#     u = sol.u[end]
-#     i += 1
-# end
+anim = Animation()
+for i in 1:SK.STEPS_PER_SEC:length(steps)
+  step = steps[i]
+  fr = SK.Plot(
+    reshape(step.u[end], SK.FIELDS, ss.n),
+    ss,
+    step.t[end],
+    vel=true,
+    nbr_springs=false,
+    nbr_alg_ds=true,
+    nbr_alg_as=false);
+  frame(anim, fr);
+end
+gif(anim, "no_noise.gif", fps=25, loop=0);
+println("Done with gif");
